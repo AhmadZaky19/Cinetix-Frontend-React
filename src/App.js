02b807a52ad6d060cd5f5c786621ba5c3ff18f11
AhmadZaky19 <a.zaky32@gmail.com>
2021-10-24 00:59:39 +0700

    add login and home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import BasicReact from "./pages/basic/React";
+import BasicLogin from "./pages/basic/Login";
+import BasicHome from "./pages/basic/Home";
+import BasicMovieDetail from "./pages/basic/DetailMovie";
+import Login from "./pages/auth/Login";
+import Home from "./pages/main/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      // <h1>App Page</h1>
+      <Router>
+        <Switch>
+          <Route path="/basic-react" exact component={BasicReact} />
+          <Route path="/basic-login" exact component={BasicLogin} />
+          <Route path="/basic-home" exact component={BasicHome} />
+          <Route path="/basic-detail/:movieId" exact component={BasicMovieDetail} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/home" exact component={Home} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
