273612bb57610b140bf4f2cc84b57b507d57cf26
AhmadZaky19 <a.zaky32@gmail.com>
2021-12-20 14:30:01 +0700

    change class component to functional component

--- src/components/Forms/Login/index.jsx
+++ src/components/Forms/Login/index.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
-import { withRouter, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { withRouter, Link, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import { compose } from "redux";
+import { toast, ToastContainer } from "react-toastify";
 import { login } from "../../../stores/actions/auth";
 import { getDataUser } from "../../../stores/actions/user";
 import styles from "./FormLogin.module.css";
@@ -9,135 +10,150 @@ import TickitzPurple from "../../../assets/img/tickitz purple.png";
 import Google from "../../../assets/img/google.png";
 import Facebook from "../../../assets/img/facebook.png";
 
-class FormLogin extends Component {
-  constructor() {
-    super();
-    this.state = {
-      form: {
-        email: "",
-        password: ""
-      },
-      isError: false
-    };
-  }
+const FormLogin = (props) => {
+  const history = useHistory();
 
-  handleChangeForm = (event) => {
-    this.setState({
-      form: {
-        ...this.state.form,
-        [event.target.name]: event.target.value
-      }
-    });
+  const [formLogin, setFormLogin] = useState({
+    email: "",
+    password: ""
+  });
+
+  const [isError, setError] = useState(props.auth.msg);
+
+  const handleChangeForm = (event) => {
+    setFormLogin({ ...formLogin, [event.target.name]: event.target.value });
   };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props
-      .login(this.state.form)
-      .then((res) => {
-        localStorage.setItem("token", res.value.data.data.token);
-        this.props.getDataUser(res.value.data.data.id).then((res) => {
-          localStorage.setItem("role", res.value.data.data[0].role);
-          if (res.value.data.data[0].role === "admin") {
-            this.props.history.push("/manage-movie");
-          } else {
-            this.props.history.push("/home");
-          }
-        });
-      })
-      .catch((err) => {
-        this.setState({
-          isError: true
-        });
-        setTimeout(() => {
-          this.setState({
-            isError: false
-          });
-        }, 3000);
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   this.props
+  //     .login(this.state.form)
+  //     .then((res) => {
+  //       localStorage.setItem("token", res.value.data.data.token);
+  //       this.props.getDataUser(res.value.data.data.id).then((res) => {
+  //         localStorage.setItem("role", res.value.data.data[0].role);
+  //         if (res.value.data.data[0].role === "admin") {
+  //           this.props.history.push("/manage-movie");
+  //         } else {
+  //           this.props.history.push("/home");
+  //         }
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       this.setState({
+  //         isError: true
+  //       });
+  //       setTimeout(() => {
+  //         this.setState({
+  //           isError: false
+  //         });
+  //       }, 3000);
+  //     });
+  // };
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      const response = await props.login(formLogin);
+      setError(response.value.data.msg);
+      localStorage.setItem("token", response.value.data.data.token);
+      localStorage.setItem("refreshToken", response.value.data.data.refreshToken);
+      props.getDataUser(response.value.data.data.id).then((res) => {
+        localStorage.setItem("role", res.value.data.data[0].role);
+        let role = res.value.data.data[0].role;
+        if (role === "admin") {
+          setTimeout(() => {
+            history.push("/manage-movie");
+          }, 2000);
+        } else {
+          setTimeout(() => {
+            history.push("/home");
+          }, 2000);
+        }
       });
+      toast.success(`${response.value.data.msg}`);
+    } catch (error) {
+      toast.error(`${error.response.data.msg}`);
+    }
   };
 
-  handleReset = (event) => {
+  const handleReset = (event) => {
     event.preventDefault();
   };
 
-  render() {
-    const { isError, msg } = this.props.auth;
-    return (
-      <div className={`${styles.form}`}>
-        <div className={`${styles.form__input}`}>
-          <div className={`${styles.mobile__only}`}>
-            <img src={TickitzPurple} alt="" />
-          </div>
-          <h1>Sign In</h1>
-          <p>Sign in with your data that you entered during your registration</p>
+  return (
+    <div className={`${styles.form}`}>
+      <div className={`${styles.form__input}`}>
+        <div className={`${styles.mobile__only}`}>
+          <img src={TickitzPurple} alt="" />
         </div>
-        <div>
-          {this.state.isError && <div className="alert alert-danger">{msg}</div>}
-          <form onSubmit={this.handleSubmit} onReset={this.handleReset}>
-            <div className={`${styles.field__input} mb-3`}>
-              <label htmlFor="email" className="form-label">
-                Email
-              </label>
-              <input
-                type="email"
-                className={`${styles.formIn} form-control`}
-                placeholder="Write your email"
-                name="email"
-                onChange={this.handleChangeForm}
-              />{" "}
-            </div>
-            <div className={`${styles.field__input} mb-3`}>
-              <label htmlFor="password" className="form-label">
-                Password
-              </label>
-              <input
-                type="password"
-                className={`${styles.formIn} form-control`}
-                placeholder="Write your password"
-                name="password"
-                onChange={this.handleChangeForm}
-              />
-              <i className="bi bi-eye"></i>
-            </div>
-            <button className={`${styles.SignIn} btn btn-primary`} type="submit">
-              Sign in
-            </button>
-          </form>
-          <div className={`${styles.reset}`}>
-            <p>
-              Not have an account? <Link to="/register">Register</Link>
-            </p>
+        <h1>Sign In</h1>
+        <p>Sign in with your data that you entered during your registration</p>
+      </div>
+      <div>
+        <ToastContainer />
+        <form onSubmit={handleSubmit} onReset={handleReset}>
+          <div className={`${styles.field__input} mb-3`}>
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className={`${styles.formIn} form-control`}
+              placeholder="Write your email"
+              name="email"
+              onChange={handleChangeForm}
+            />{" "}
           </div>
-          <div className={`${styles.reset}`}>
-            <p>
-              Forgot your password? <Link to="/reset-password">Reset now</Link>
-            </p>
+          <div className={`${styles.field__input} mb-3`}>
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className={`${styles.formIn} form-control`}
+              placeholder="Write your password"
+              name="password"
+              onChange={handleChangeForm}
+            />
+            <i className="bi bi-eye"></i>
           </div>
-        </div>
-        <div className={`${styles.section_or}`}>
-          <span className={`${styles.section_or_line}`}></span>
-          <h2>Or</h2>
-          <span className={`${styles.section_or_line}`}></span>
-        </div>
-        <div className={`${styles.link_button}`}>
-          <button role="button" className="mx-5">
-            <i>
-              <img src={Google} alt="google" />
-            </i>
-            <p>Google</p>
+          <button className={`${styles.SignIn} btn btn-primary`} type="submit">
+            Sign in
           </button>
-          <button type="button" className="mx-5">
-            <i>
-              <img src={Facebook} alt="facebook" />
-            </i>
-            <p>Facebook</p>
-          </button>
+        </form>
+        <div className={`${styles.reset}`}>
+          <p>
+            Not have an account? <Link to="/register">Register</Link>
+          </p>
+        </div>
+        <div className={`${styles.reset}`}>
+          <p>
+            Forgot your password? <Link to="/reset-password">Reset now</Link>
+          </p>
         </div>
       </div>
-    );
-  }
-}
+      <div className={`${styles.section_or}`}>
+        <span className={`${styles.section_or_line}`}></span>
+        <h2>Or</h2>
+        <span className={`${styles.section_or_line}`}></span>
+      </div>
+      <div className={`${styles.link_button}`}>
+        <button role="button" className="mx-5">
+          <i>
+            <img src={Google} alt="google" />
+          </i>
+          <p>Google</p>
+        </button>
+        <button type="button" className="mx-5">
+          <i>
+            <img src={Facebook} alt="facebook" />
+          </i>
+          <p>Facebook</p>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   auth: state.auth
--- src/pages/auth/Login/index.jsx
+++ src/pages/auth/Login/index.jsx
@@ -5,29 +5,27 @@ import Quote from "../../../assets/img/quote.png";
 import Image from "../../../assets/img/image 1.png";
 import FormLogin from "../../../components/Forms/Login";
 
-class Login extends Component {
-  render() {
-    return (
-      <div className={`${styles.contain} container-fluid`}>
-        <div className="row">
-          <div className={`${styles.column1} col-md-7 d-none d-md-inline-block`}>
-            <div className={`${styles.row__overlay}`}>
-              <span className="row__overlay--title">
-                <img src={TickitzWhite} alt="App" />
-              </span>
-              <span className={`${styles.row__overlay__quote}`}>
-                <img src={Quote} alt="Quote" />
-              </span>
-            </div>
-            <img src={Image} alt="Landing page" className={`${styles.row__image}`} />
+const Login = () => {
+  return (
+    <div className={`${styles.contain} container-fluid`}>
+      <div className="row">
+        <div className={`${styles.column1} col-md-7 d-none d-md-inline-block`}>
+          <div className={`${styles.row__overlay}`}>
+            <span className="row__overlay--title">
+              <img src={TickitzWhite} alt="App" />
+            </span>
+            <span className={`${styles.row__overlay__quote}`}>
+              <img src={Quote} alt="Quote" />
+            </span>
           </div>
-          <div className={`${styles.column2} col-md-5 col-sm-12`}>
-            <FormLogin />
-          </div>
+          <img src={Image} alt="Landing page" className={`${styles.row__image}`} />
+        </div>
+        <div className={`${styles.column2} col-md-5 col-sm-12`}>
+          <FormLogin />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Login;
