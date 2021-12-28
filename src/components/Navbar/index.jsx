63cbd1524844a758def4652498537a17fadceb0f
AhmadZaky19 <a.zaky32@gmail.com>
2021-12-28 20:32:47 +0700

    add integration for search movie

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,11 +4,53 @@ import { Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { Navbar, Container, Nav, Form, FormControl, Button, Image } from "react-bootstrap";
 import { getDataUser } from "../../stores/actions/user";
+import { getDataMovie } from "../../stores/actions/movie";
 import TickitzPurple from "../../assets/img/tickitz purple.png";
 import user from "../../assets/img/user_icon.png";
 import "./index.css";
 
 class Navigation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchMenu: false,
+      search: "",
+      movies: [],
+      message: "",
+      isError: false
+    };
+  }
+
+  handleSearch = () => {
+    this.setState({
+      searchMenu: true
+    });
+  };
+
+  handleFindMovie = (event) => {
+    this.props.getDataMovie("", this.state.search, "", "", "", "").then((response) => {
+      if (response.value.data.data.length > 0) {
+        this.setState({
+          movies: response.value.data.data,
+          isError: false,
+          message: response.value.data.msg
+        });
+      } else {
+        this.setState({
+          isError: true,
+          message: response.value.data.msg
+        });
+      }
+    });
+    this.setState({
+      search: event.target.value
+    });
+  };
+
+  handleDetailSearchMovie = (id) => {
+    this.props.history.push(`/movie-detail/${id}`);
+  };
+
   handleToRegister = () => {
     this.props.history.push("/register");
   };
@@ -46,15 +88,48 @@ class Navigation extends Component {
                   Profile
                 </Nav.Link>
               </Nav>
-              <Form className="d-flex">
-                <FormControl
-                  type="search"
-                  placeholder="Search"
-                  className="me-2 search__form"
-                  aria-label="Search"
-                />
-              </Form>
-              <Search size={24} />
+
+              <Button
+                // variant="outline-light"
+                className="button__search"
+                onClick={this.handleSearch}
+              >
+                {this.state.searchMenu ? (
+                  <>
+                    <Form className="d-flex">
+                      <FormControl
+                        type="search"
+                        placeholder="Search movie..."
+                        className="me-2 d-none d-md-flex search__input"
+                        onChange={this.handleFindMovie}
+                      />
+                    </Form>
+                    {this.state.search ? (
+                      <div className="navigation__homepage-search-movie">
+                        <p className="text-dark fw-bold">Search: {this.state.search}</p>
+                        <hr className="text-dark" />
+                        {!this.state.isError ? (
+                          this.state.movies.map((item) => (
+                            <div key={item.id}>
+                              <button
+                                className="navigation__homepage-search-movie-link text-decoration-none"
+                                onClick={() => this.handleDetailSearchMovie(item.id)}
+                              >
+                                {item.name}
+                              </button>
+                            </div>
+                          ))
+                        ) : (
+                          <p className="text-dark fw-bold mx-5">{this.state.message}</p>
+                        )}
+                        <hr />
+                      </div>
+                    ) : null}
+                  </>
+                ) : (
+                  <Search size={24} style={{ color: "black" }} />
+                )}
+              </Button>
               {token !== null ? (
                 <Image
                   src={
@@ -88,11 +163,13 @@ class Navigation extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  user: state.user
+  user: state.user,
+  movie: state.movie
 });
 
 const mapDispatchToProps = {
-  getDataUser
+  getDataUser,
+  getDataMovie
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Navigation));
