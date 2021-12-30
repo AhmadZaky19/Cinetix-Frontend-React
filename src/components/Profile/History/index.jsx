6f38394b6ac6c8e59354616372da146504997bf0
AhmadZaky19 <a.zaky32@gmail.com>
2021-12-30 12:40:14 +0700

    add integration to order history

--- src/components/Profile/History/index.jsx
+++ src/components/Profile/History/index.jsx
@@ -1,64 +1,100 @@
-import React from "react";
-import { Row, Col, Form, Button, Card } from "react-bootstrap";
-import Ebv from "../../../assets/img/ebuid.png";
-import CineOne from "../../../assets/img/cineone.png";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { connect } from "react-redux";
+import moment from "moment";
+import { Row, Col, Button, Card } from "react-bootstrap";
+import { getDataBookingByUserId } from "../../../stores/actions/booking";
+import CineOne21 from "../../../assets/img/cineone.png";
 import Hiflix from "../../../assets/img/hiflix.png";
+import EbvId from "../../../assets/img/ebuid.png";
 import "./index.css";
 
-const History = () => {
+const History = (props) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const [data, setData] = useState([]);
+
+  const getBookingByUserId = () => {
+    dispatch(getDataBookingByUserId(props.user.dataUser.id)).then((res) => {
+      setData(res.value.data.data);
+    });
+  };
+
+  useEffect(() => {
+    getBookingByUserId();
+  }, []);
   return (
     <>
       <div className="user__data--orderHistory">
-        <Card class="order__history">
-          <Row>
-            <Col md={9} className="order__history--desc">
-              <p style={{ color: "#AAAAAA" }}>Tuesday, 07 July 2020 - 04:30pm</p>
-              <h4>Spider-man: Homecoming</h4>
-            </Col>
-            <Col md={3}>
-              {" "}
-              <Card.Img variant="top" src={Ebv} className="order__history--image" />
-            </Col>
-          </Row>
-          <hr />
-          <Card.Body>
-            <div className="order__history--ticket">
-              <Button variant="success" className="order__history--button1 mx-2">
-                Ticket in active
-              </Button>
-              <div className="order__history--details">
-                <h5>Show Details</h5>
-              </div>
-            </div>
-          </Card.Body>
-        </Card>
-        <br />
-        <Card class="order__history">
-          <Row>
-            <Col md={9} className="order__history--desc">
-              <p style={{ color: "#AAAAAA" }}>Tuesday, 07 July 2020 - 04:30pm</p>
-              <h4>Spider-man: Homecoming</h4>
-            </Col>
-            <Col md={3}>
-              {" "}
-              <Card.Img variant="top" src={Hiflix} className="order__history--image" />
-            </Col>
-          </Row>
-          <hr />
-          <Card.Body>
-            <div className="order__history--ticket">
-              <Button variant="success" className="order__history--button2 mx-2">
-                Ticket used
-              </Button>
-              <div className="order__history--details">
-                <h5>Show Details</h5>
-              </div>
-            </div>
-          </Card.Body>
-        </Card>
+        {data.length > 0 ? (
+          data.map((item) => (
+            <>
+              <Card className="order__history" key={item.id}>
+                <Row className="mx-4">
+                  <Col md={9} className="order__history--desc">
+                    <p style={{ color: "#AAAAAA" }}>
+                      {moment(item.dateBooking).format("dddd, MMMM Do YYYY")} -{" "}
+                      {item.timeBooking.substring(0, 5)}
+                    </p>
+                    <h4>{item.name}</h4>
+                  </Col>
+                  <Col md={3}>
+                    {" "}
+                    <div className="order__history--image">
+                      <Card.Img
+                        variant="top"
+                        src={
+                          item.premiere === "ebu.id"
+                            ? EbvId
+                            : data.premiere === "hiflix"
+                            ? Hiflix
+                            : CineOne21
+                        }
+                      />
+                    </div>
+                  </Col>
+                </Row>
+                <hr />
+                <Card.Body>
+                  <div className="order__history--ticket">
+                    {item.statusUsed === "active" ? (
+                      <Button className="order__history--button1 mx-2" disabled>
+                        Ticket in active
+                      </Button>
+                    ) : (
+                      <Button className="order__history--button2 mx-2" disabled>
+                        Ticket used
+                      </Button>
+                    )}
+
+                    <div className="order__history--details">
+                      <h5>Show Details</h5>
+                    </div>
+                  </div>
+                </Card.Body>
+              </Card>
+              <br />
+            </>
+          ))
+        ) : (
+          <div className="coming-soon">
+            <h1>No Order History</h1>
+          </div>
+        )}
       </div>
     </>
   );
 };
 
-export default History;
+const mapStateToProps = (state) => ({
+  booking: state.booking,
+  user: state.user
+});
+
+const mapDispatchToProps = {
+  getDataBookingByUserId
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(History);
