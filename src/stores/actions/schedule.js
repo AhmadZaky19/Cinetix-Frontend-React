74526411af73ad2d51b3d8c4350ea9afee0406c8
AhmadZaky19 <a.zaky32@gmail.com>
2022-01-10 20:46:15 +0700

    change actions name

--- src/stores/actions/schedule.js
+++ src/stores/actions/schedule.js
@@ -2,7 +2,7 @@ import axios from "../../utils/axios";
 
 export const getSchedule = (location, movieId, sort, order, page, limit) => {
   return {
-    type: "GET_SCHEDULE_BY_ID",
+    type: "GET_SCHEDULE",
     payload: axios.get(
       `schedule/?location=${location}&movieId=${movieId}&sort=${sort}&order=${order}&page=${page}&limit=${limit}`
     )
